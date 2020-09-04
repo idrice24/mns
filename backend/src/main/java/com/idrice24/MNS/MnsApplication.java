8ea17eae15792bf4d0a5113b9d35286a7e4019c3
idrice24 <50774032+idrice24@users.noreply.github.com>
2020-09-04 14:16:14 +0100

    backend initialize
    the backend is ready to start

---
+++ backend/src/main/java/com/idrice24/MNS/MnsApplication.java
@@ -0,0 +1,13 @@
+package com.idrice24.MNS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/idrice24/MNS/MnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idrice24.MNS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
