e1f114a3892077e07ac6721f7861171afa9eaabe
idrice24 <50774032+idrice24@users.noreply.github.com>
2020-09-07 13:48:38 +0100

    Update MnsApplication.java

--- backend/src/main/java/com/idrice24/MNS/MnsApplication.java
+++ backend/src/main/java/com/idrice24/MNS/MnsApplication.java
@@ -3,7 +3,6 @@ package com.idrice24.MNS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class MnsApplication {
 
 	public static void main(String[] args) {
