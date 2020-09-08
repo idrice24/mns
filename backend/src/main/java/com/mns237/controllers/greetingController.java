a33eee84cc67deeba0a9ad03607c4b6cb9bf44ff
Ghislain <Gzeleu@gmx.de>
2020-09-08 18:02:15 +0200

    Fix error  by adding artifach version of parent

--- backend/src/main/java/com/mns237/controllers/greetingController.java
+++ backend/src/main/java/com/mns237/controllers/greetingController.java
@@ -8,11 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class greetingController{
-    private static final String template = "hello, %s";
-    private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("greeting")
-    public Greeting greeting(@RequestParam(value ="name", defaultValue = "World") String name){
-        return new Greeting(counter.incrementAndGet(). String.format(template, name));
-    }
 }
