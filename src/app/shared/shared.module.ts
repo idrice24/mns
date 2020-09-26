070b8ac236405f7510188fa5061e6830cf14fab0
Ghislain <gzeleu@outlook.de>
2020-09-26 14:23:16 +0200

    [PR]  for #106 And #121 (#124)
    * (#123) header and Footer added
    
    * (#106) Part 1 for blog impl

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HeaderComponent } from './components/header/header.component';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
+  exports: [HeaderComponent, FooterComponent],
   providers: []
 })
 export class SharedModule { }
