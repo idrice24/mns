8a7f32357415862204db46f67ab68f628f450a4c
Ghislain <Gzeleu@gmx.de>
2020-10-02 17:34:42 +0200

    #135 Add login and regsrty possibiy in ui web

---
+++ src/app/core/components/registry/registry.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registry',
+  templateUrl: './registry.component.html',
+  styleUrls: ['./registry.component.css']
+})
+export class RegistryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
