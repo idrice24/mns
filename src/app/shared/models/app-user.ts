aeb2eee781e629b536844dfb904706250f43b9b6
Ghislain <Gzeleu@gmx.de>
2020-10-02 17:05:59 +0200

    #135 Add login and regsrty possibiy in ui web

---
+++ src/app/shared/models/app-user.ts
@@ -0,0 +1,7 @@
+// https://github.com/mosh-hamedani/organic-shop/blob/master/src/app/shared/models/app-user.ts
+export interface AppUser {
+  name: string;
+  email: string;
+  isAdmin: boolean;
+}
+
