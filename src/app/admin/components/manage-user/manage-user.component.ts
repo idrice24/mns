a2e93fd8f5a80608925277840a6e7902b1efe770
Ghislain <Gzeleu@gmx.de>
2020-10-04 00:56:34 +0200

    (#163) Admin the one that can displaya users list

---
+++ src/app/admin/components/manage-user/manage-user.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/shared/services/user.service';
+import { AppUser } from 'src/app/shared/models/app-user';
+
+@Component({
+  selector: 'app-manage-user',
+  templateUrl: './manage-user.component.html',
+  styleUrls: ['./manage-user.component.css']
+})
+export class ManageUserComponent implements OnInit {
+
+  users: AppUser[];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+  getUsers(): void {
+    this.userService.getUsers()
+      .subscribe(users => this.users = users);
+  }
+}
