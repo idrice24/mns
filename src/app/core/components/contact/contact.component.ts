abff4a817a96181a6538aa2c48098d4df727a096
idrice24 <idrice.tsafouet@yahoo.com>
2020-10-06 18:05:57 +0100

    Idrice1 (#171)
    * Update config.yml issue #162
    
    * Update karma.confi issue #162
    
    * Update karma issue #162
    
    * Update config.yml issue #162
    
    * Update login form issue #155
    
    * Create login2.jpg
    
    * Update karma.conf.js
    
    * Update config.yml
    
    * Update login issue #155
    
    * update files issue #151
    
    * update contact issue #151
    
    * update contact issue #151
    
    * update register issue #155
    
    * updating login form issue #155

--- src/app/core/components/contact/contact.component.ts
+++ src/app/core/components/contact/contact.component.ts
@@ -1,13 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+// import {} from 'googlemaps';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent  {
+	title = 'mns+237-gmap';
+
+	// @ViewChild('mapContainer' , {static: false}) gmap: ElementRef;
+  //	map: google.maps.Map;
+
+  	lat = 40.730610;
+  	lng = -73.935242;
+
+	// coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+  	// mapOptions: google.maps.MapOptions = {
+  		// center: this.coordinates,
+  	// zoom: 8,
+  	// };
+
+  // marker = new google.maps.Marker({
+  //   position: this.coordinates,
+  //   map: this.map,
+  // 	});
+
+	
+  
+	//mapInitialiter(){
+  // 	this.map = new google.maps.Map(this.gmap.nativeElement,
+  // 	this.mapOptions,
+  // 	this.marker.setMap(this.map));
+  //	}
+
+	//ngAfterViewInit(){
+  //	this.mapInitialiter();
+  //	}
 
   constructor() { }
+  						
 
   ngOnInit(): void {
   }
