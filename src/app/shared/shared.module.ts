b3bc72e95702aa510227df83993ffabad8e2d0dc
Ghislain <Gzeleu@gmx.de>
2020-10-06 23:49:18 +0200

    #162) Configure CLI for CI testing in Chrome

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from '../in-memory-data.service';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
