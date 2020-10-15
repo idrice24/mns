4cbb4d65e11ca3c4e62104d56562568ead3aa2ba
idrice24 <50774032+idrice24@users.noreply.github.com>
2020-10-15 14:52:04 +0100

    add more blogs issue #189

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -14,6 +14,7 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 13, fName: 'Zoe', lName: 'Dr Nice', email: 'Zoe@yahoo.com', password: 'password1', isAdmin: false },
 
     ];
+    
     const blog = [
       {
         _embedded: {
@@ -31,6 +32,26 @@ export class InMemoryDataService implements InMemoryDbService {
             createdDate: '2013-03-02T16:41:45Z',
             id: 3,
             imageUrl: 'assets/img/aboutus.jpg',
+            publishedDate: '2020-03-02T16:41:00Z',
+            title: 'third topic',
+            updatedDate: '2020-03-02T16:41:45Z',
+            summary: 'this is the second blog...hi... this is the second blog.is the first blog.hi this is the first blog.hi this is the first blog.hi this is the first blog.hi this is the first blog.hi this is the first blog.hi this is the first blog.hi this is the'
+          },
+          {
+            content: 'hi... this is the third blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... ..hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...',
+            createdDate: '2013-03-02T16:41:45Z',
+            id: 3,
+            imageUrl: 'assets/img/CMR5.jpg',
+            publishedDate: '2020-03-02T16:41:00Z',
+            title: 'third topic',
+            updatedDate: '2020-03-02T16:41:45Z',
+            summary: 'this is the second blog...hi... this is the second blog.is the first blog.hi this is the first blog.hi this is the first blog.hi this is the first blog.hi this is the first blog.hi this is the first blog.hi this is the first blog.hi this is the'
+          },
+          {
+            content: 'hi... this is the fouth blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... ..hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...',
+            createdDate: '2013-03-02T16:41:45Z',
+            id: 3,
+            imageUrl: 'assets/img/CMR7.jpg',
             publishedDate: '2020-03-02T16:41:00Z',
             title: 'third topic',
             updatedDate: '2020-03-02T16:41:45Z',
