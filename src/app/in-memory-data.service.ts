abe41022833ffca90eb6a2feebbe15bf515b3c29
idrice24 <idrice.tsafouet@yahoo.com>
2020-10-16 16:50:00 +0100

    Idrice2 (#217)
    * Update package-lock.json
    
    * adding dummpy images issue #194
    
    * aline home icon issue #187
    
    * add video and gallery icon #194
    
    * issue #195
    
    * service for videos issue #196
    
    * add more blogs issue #189
    
    * update files issue #187
    
    * issue #215

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -2,6 +2,7 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { AppUser } from './shared/models/app-user';
+import { AppVideo } from './shared/models/app-video';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,13 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 12, fName: 'Bombasto', lName: 'Dr Nice', email: 'Bombasto@yahoo.com', password: 'password1', isAdmin: false },
       { id: 13, fName: 'Zoe', lName: 'Dr Nice', email: 'Zoe@yahoo.com', password: 'password1', isAdmin: false },
 
+    ];
+
+    const videos = [
+      { id: 1, name: 'CR goals', publishedDate: '2020/10/13', title: 'CR7 First Goal in UV', createdDate: '2020/10/10' },
+      { id: 2, name: 'CR goals', publishedDate: '2020/10/13', title: 'CR7 First Goal in UV', createdDate: '2020/10/10' },
+      { id: 3, name: 'CR goals', publishedDate: '2020/10/13', title: 'CR7 First Goal in UV', createdDate: '2020/10/10' },
+      { id: 4, name: 'CR goals', publishedDate: '2020/10/13', title: 'CR7 First Goal in UV', createdDate: '2020/10/10' },
     ];
     const blog = [
       {
@@ -31,6 +39,26 @@ export class InMemoryDataService implements InMemoryDbService {
             createdDate: '2013-03-02T16:41:45Z',
             id: 3,
             imageUrl: 'assets/img/aboutus.jpg',
+            publishedDate: '2020-03-02T16:41:00Z',
+            title: 'third topic',
+            updatedDate: '2020-03-02T16:41:45Z',
+            summary: 'this is the second blog...hi... this is the second blog.is the first blog.hi this is the first blog.hi this is the first blog.hi this is the first blog.hi this is the first blog.hi this is the first blog.hi this is the first blog.hi this is the'
+          },
+          {
+            content: 'hi... this is the third blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... ..hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...',
+            createdDate: '2013-03-02T16:41:45Z',
+            id: 3,
+            imageUrl: 'assets/img/CMR5.jpg',
+            publishedDate: '2020-03-02T16:41:00Z',
+            title: 'third topic',
+            updatedDate: '2020-03-02T16:41:45Z',
+            summary: 'this is the second blog...hi... this is the second blog.is the first blog.hi this is the first blog.hi this is the first blog.hi this is the first blog.hi this is the first blog.hi this is the first blog.hi this is the first blog.hi this is the'
+          },
+          {
+            content: 'hi... this is the fouth blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... ..hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...hi... this is the second blog...',
+            createdDate: '2013-03-02T16:41:45Z',
+            id: 3,
+            imageUrl: 'assets/img/CMR7.jpg',
             publishedDate: '2020-03-02T16:41:00Z',
             title: 'third topic',
             updatedDate: '2020-03-02T16:41:45Z',
