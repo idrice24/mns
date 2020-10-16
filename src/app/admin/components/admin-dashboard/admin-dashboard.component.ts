541a507aaf9cf189090a2658c1ce8222f5598145
Ghislain <Gzeleu@gmx.de>
2020-10-16 21:32:02 +0200

    Improve adminn dashbrdr #192

--- src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LogService } from 'src/app/shared/services/log.service';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardComponent implements OnInit {
 
-  constructor() { }
+  items;
+  constructor(private logService: LogService) { }
 
   ngOnInit(): void {
+    this.items = [
+      {
+        text1: 'oka',
+        text2: 'oka',
+        src: '/assets/img/bg1.jpg',
+        title: 'Admin Action 1'
+      },
+      {
+        text1: 'oka',
+        text2: 'oka',
+        src: '/assets/img/bg4.jpg',
+        title: 'Admin Action 1'
+      },
+      {
+        text1: 'Un ext por explique ce que admin dot faire',
+        text2: 'Last updated 3 mins ago',
+        src: 'assets/img/aboutus.jpg',
+        title: 'Admin Action 1'
+      }
+    ];
+    this.logService.log(this.items);
   }
 
 }
