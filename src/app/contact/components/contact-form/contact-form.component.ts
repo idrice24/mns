d169d18e59062ea8e06c3ac6cefc70b06168323c
Ghislain <Gzeleu@gmx.de>
2020-10-18 20:52:48 +0200

    .

--- src/app/contact/components/contact-form/contact-form.component.ts
+++ src/app/contact/components/contact-form/contact-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { AppUser } from 'src/app/shared/models/app-user';
+
 
 @Component({
   selector: 'app-contact-form',
@@ -6,10 +10,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
+  message: string;
+  sendForm: FormGroup;
+  appUser: AppUser;
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit(): void {
+    // TODO@Idrice api  by sending email
+    this.appUser = {
+      email: '',
+      fName: '',
+      isAdmin: false,
+      lName: '',
+      password: '',
+      id: 0
+    };
   }
 
+  ngOnInit(): void {
+    this.sendForm = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+      email: new FormControl(this.appUser.email, [
+        Validators.required,
+        Validators.minLength(4)
+      ])
+    }
+    );
+  }
+
+
+  // convenience getter for easy access to form fields
+  get name() { return this.sendForm.get('name'); }
+  get email() { return this.sendForm.get('email'); }
+
+
+  send(data) {
+    console.warn('Your order has been submitted', data);
+    this.showSuccess();
+  }
+  private showSuccess() {
+    console.log('YYYYYYYYYYY');
+    // this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Message Content' });
+  }
 }
