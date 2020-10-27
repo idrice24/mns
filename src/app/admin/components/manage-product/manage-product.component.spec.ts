eb36b32dfec7925d66d41441e6aaf54508dda6ae
Ghislain <Gzeleu@gmx.de>
2020-10-27 20:31:29 +0100

    ADD MOCLK PRODUCT DATA

--- src/app/admin/components/manage-product/manage-product.component.spec.ts
+++ src/app/admin/components/manage-product/manage-product.component.spec.ts
@@ -1,22 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ManageProductComponent } from './manage-product.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ProductService } from 'src/app/shared/services/product.service';
+import { ConfirmationService, MessageService } from 'primeng/api';
+
 
 describe('ManageProductComponent', () => {
   let component: ManageProductComponent;
   let fixture: ComponentFixture<ManageProductComponent>;
+  let productService: ProductService;
+  let messageService: MessageService;
+  let confirmationService: ConfirmationService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ManageProductComponent ]
+      declarations: [ManageProductComponent],
+      imports: [HttpClientTestingModule,
+        FormsModule,
+        RouterTestingModule],
+      providers: [MessageService, ConfirmationService
+
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
+    productService = TestBed.inject(ProductService);
+    messageService = TestBed.inject(MessageService);
+    confirmationService = TestBed.inject(ConfirmationService);
+
     fixture = TestBed.createComponent(ManageProductComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
   });
 
   it('should create', () => {
--- src/app/shared/services/product.service.spec.ts
+++ src/app/shared/services/product.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './product.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('ProductService', () => {
   let service: ProductService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ imports: [HttpClientModule] });
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule,
+        HttpClientTestingModule,
+        ReactiveFormsModule]
+    });
+
     service = TestBed.inject(ProductService);
   });
 
