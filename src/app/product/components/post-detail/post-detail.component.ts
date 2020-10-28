601518523a19b7dd890e30ef614c3ad775928620
Ghislain <Gzeleu@gmx.de>
2020-10-28 17:47:44 +0100

    UPDATE GITUHP DUE

--- src/app/product/components/post-detail/post-detail.component.ts
+++ src/app/product/components/post-detail/post-detail.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
-  styleUrls: ['./post-detail.component.css']
+  styleUrls: ['./post-detail.component.scss']
 })
 export class PostDetailComponent implements OnInit {
 
--- src/app/product/components/product-list/product-list.component.ts
+++ src/app/product/components/product-list/product-list.component.ts
@@ -6,7 +6,7 @@ import { ProductService } from 'src/app/shared/services/product.service';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
 
