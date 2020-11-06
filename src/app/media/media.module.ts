df28b9281fb311185ecfe7dee578eb8eba923716
idrice24 <50774032+idrice24@users.noreply.github.com>
2020-11-06 09:50:53 +0100

    update gallery module #290

--- src/app/media/media.module.ts
+++ src/app/media/media.module.ts
@@ -7,6 +7,7 @@ import { GalleryComponent } from './components/gallery/gallery.component';
 import { MediaDashboardComponent } from './components/media-dashboard/media-dashboard.component';
 import { TableModule, Table } from 'primeng/table';
 import { SharedModule } from '../shared/shared.module';
+import { FileUploadModule } from 'primeng/fileupload';
 
 import { GalleriaModule } from 'primeng/galleria';
 import { CardModule } from 'primeng/card';
@@ -29,6 +30,7 @@ const mediasRoutes: Routes = [
     SharedModule,
     GalleriaModule,
     CardModule,
+    FileUploadModule,
     RouterModule.forChild(mediasRoutes)
   ],
   exports: [RouterModule]
