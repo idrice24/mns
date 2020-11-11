9a954cce2aa000bfeeb0bce4e40987586473d04b
idrice24 <50774032+idrice24@users.noreply.github.com>
2020-11-11 16:27:56 +0100

    update blog service #281

--- src/app/shared/services/blog.service.ts
+++ src/app/shared/services/blog.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { GetResponse } from '../models/get-response';
-import { map } from 'rxjs/operators';
 import { Topic } from '../models/topic';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { tap, catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +12,9 @@ export class BlogService {
 
   // private baseUrl = '/assets/data/blogs.json';
   private blogUrl = 'api/blog';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(private httpClient: HttpClient) { }
 
@@ -28,5 +31,36 @@ export class BlogService {
       map((topics: Topic[]) => topics.find(topic => topic.id === +id))
 
     );
+  }
+
+  /** POST: add a new Blog to the server */
+  addBlog(Topic: Topic): Observable<Topic> {
+    return this.httpClient.post<Topic>(this.blogUrl, Topic, this.httpOptions).pipe(
+      tap((newBlog: Topic) => this.log(`added Blog w/ id=${newBlog.id}`)),
+      catchError(this.handleError<Topic>('addBlog'))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console instead
+
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a BlogService message with the MessageService */
+  private log(message: string) {
+    // this.messageService.add(`BlogService: ${message}`);
+    console.warn(`BlogService: ${message}`);
   }
 }
