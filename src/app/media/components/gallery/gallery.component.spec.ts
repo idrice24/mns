9c88a96fe6c2f3f37391242536c8f166800b507e
idrice24 <50774032+idrice24@users.noreply.github.com>
2020-11-18 16:13:36 +0100

    udate files

--- src/app/media/components/gallery/gallery.component.spec.ts
+++ src/app/media/components/gallery/gallery.component.spec.ts
@@ -1,5 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { GalleryComponent } from './gallery.component';
 
