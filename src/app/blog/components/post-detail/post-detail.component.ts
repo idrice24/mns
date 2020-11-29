5a3266c5df96b3357daac6fbcadaf3afd2555a13
idrice24 <50774032+idrice24@users.noreply.github.com>
2020-11-29 23:42:33 +0100

    udating the post component #268
    to add create comment and get commennt function

--- src/app/blog/components/post-detail/post-detail.component.ts
+++ src/app/blog/components/post-detail/post-detail.component.ts
@@ -4,7 +4,7 @@ import { map } from 'rxjs/operators';
 import { Topic } from 'src/app/shared/models/topic';
 import { Comment } from 'src/app/shared/models/app-comment';
 import { BlogService } from 'src/app/shared/services/blog.service';
-import { FormControl, FormGroup, Validators, FormBuilder, FormArray } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post-detail',
@@ -15,78 +15,65 @@ export class PostDetailComponent implements OnInit {
 
 
   public header: any;
-  private comment: Comment;
   public commentForm: FormGroup;
-  private comments: Comment[];
+
 
   public selectedTopic: Topic;
   blogs: Topic[];
   blog: Topic;
+  appCommentList: Topic[];
 
   // @Irice why should i use ActivatedRoute  in this Component?
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
-    private blogService: BlogService,
-    private fb: FormBuilder) {
-    this.createForm(); }
-
-      // this is to create a form for user in order to comment in groups
-    createForm(){
-    this.commentForm = this.fb.group({
-    name: [null, Validators.required],
-    code: [null, [Validators.required, Validators.minLength(2)]],
-    comment: [null, [Validators.required, Validators.minLength(20)]],
-    notablePeople: this.fb.array([])
-    });
-    }
-    get notablePeople(): FormArray {
-    return this.commentForm.get('notablePeople') as FormArray;
-    }
-// add people
-    addNotablePerson(){
-    this.notablePeople.push(this.fb.group({
-    name: ['', Validators.required],
-    title: ['', Validators.required],
-    email: ['', Validators.required]
-    }));
-    }
-removeNotablePerson(index: number){
-  this.notablePeople.removeAt(index);
-}
-
-resetForm(){
-  this.commentForm.reset();
-}
-
-onSubmit(){
-  this.comment = Object.assign({}, this.commentForm.value);
-  console.log('Saving comment', this.comment);
-}
+    private blogService: BlogService) { }
 
   ngOnInit(): void {
 
     this.getCurrentTopic();
-    // default data binding for comment
-    this.comments = [
-    new Comment('idrice', 0, 2,  2,  'idrice.tsafouet@yahoo.com', 'this is my first comment', '')];
-
-    this.getComment();
+    this.blogService.getAppComment().subscribe(i => this.appCommentList = i);
+    this.commentForm = new FormGroup({
+      comment: new FormControl('', [
+        Validators.required,
+        Validators.minLength(20),
+      ]),
+    });
 
   }
 
-  getComment(): Comment[] {
-  return this.comments;
+  // Save to data base this user information
+  doSubscription() {
+
+    const currentAppComment: Topic = {
+      comment: this.comment.value,
+      verified: false
+    };
+
+    this.blogService.addAppComment(currentAppComment).subscribe(_ =>
+      // To Clean a formular
+      this.commentForm.reset());
+
   }
+
+  // convenience getter for easy access to form fields
+  get comment() { return this.commentForm.get('comment'); }
+
 // creating a comment by pushing it
-  createComment(comment: Comment){
-  const foundComment = this.comments.find(each => each.code === this.blog.id);
-  if (foundComment){
-  return false;
-  }
-  this.comments.push(comment);
-  return true;
-  }
+// still under proceed wting for the service files
+  // createComment(commentForm){
+  //   if(commentForm.valid){
+  //     const created = this.blogService.createComment( this.comment );
+  //     if(created){
+  //       this.message = '' + this.counter++;
+  //       this.comment = new Comment('', 0, '',);
+  //     }else{
+  //       this.message = 'Not commented!!';
+  //     }
+  //   }else{
+  //     console.error('Comments had a problem!!');
+  //   }
+  // }
 
   private getCurrentTopic(): void {
     this.activatedRoute.params
