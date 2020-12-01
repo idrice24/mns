990227346170066635b927b4457acd946dd981b0
idrice24 <50774032+idrice24@users.noreply.github.com>
2020-12-01 10:59:31 +0100

    update post details file #268
    this is to add the comment function

--- src/app/blog/components/post-detail/post-detail.component.ts
+++ src/app/blog/components/post-detail/post-detail.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { Topic } from 'src/app/shared/models/topic';
 import { Comment } from 'src/app/shared/models/app-comment';
 import { BlogService } from 'src/app/shared/services/blog.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-post-detail',
@@ -22,6 +23,7 @@ export class PostDetailComponent implements OnInit {
   blogs: Topic[];
   blog: Topic;
   appCommentList: Topic[];
+  public appCommentLists: Topic;
 
   // @Irice why should i use ActivatedRoute  in this Component?
   constructor(
@@ -32,7 +34,7 @@ export class PostDetailComponent implements OnInit {
   ngOnInit(): void {
 
     this.getCurrentTopic();
-    this.blogService.getAppComment().subscribe(i => this.appCommentList = i);
+    this.getCurrentComment();
     this.commentForm = new FormGroup({
       comment: new FormControl('', [
         Validators.required,
@@ -43,13 +45,15 @@ export class PostDetailComponent implements OnInit {
   }
 
   // Save to data base this user information
+  /** CREATE: create a comment */
   doSubscription() {
+
 
     const currentAppComment: Topic = {
       comment: this.comment.value,
       verified: false
     };
-
+    this.blogService.addAppComment(currentAppComment).subscribe();
     this.blogService.addAppComment(currentAppComment).subscribe(_ =>
       // To Clean a formular
       this.commentForm.reset());
@@ -59,21 +63,20 @@ export class PostDetailComponent implements OnInit {
   // convenience getter for easy access to form fields
   get comment() { return this.commentForm.get('comment'); }
 
-// creating a comment by pushing it
-// still under proceed wting for the service files
-  // createComment(commentForm){
-  //   if(commentForm.valid){
-  //     const created = this.blogService.createComment( this.comment );
-  //     if(created){
-  //       this.message = '' + this.counter++;
-  //       this.comment = new Comment('', 0, '',);
-  //     }else{
-  //       this.message = 'Not commented!!';
-  //     }
-  //   }else{
-  //     console.error('Comments had a problem!!');
-  //   }
-  // }
+
+  private getCurrentComment(): void{
+  this.activatedRoute.params.subscribe(params => {
+    if (params.id !== undefined ){
+      this.blogService.getTopicById(params.id).subscribe(data => {
+        if ((data !== null) && (data !== undefined)){
+          this.appCommentLists = data;
+            }else{
+              this.router.navigate(['/posts']);
+                  }
+            });
+         }
+  });
+  }
 
   private getCurrentTopic(): void {
     this.activatedRoute.params
@@ -81,7 +84,6 @@ export class PostDetailComponent implements OnInit {
         if (params.id !== undefined) {
           this.blogService.getTopicById(params.id)
             .subscribe(data => {
-
               if ((data !== null) && (data !== undefined)) {
                 this.header = data.title;
                 this.selectedTopic = data;
