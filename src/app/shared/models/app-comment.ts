9cce8e687116e03200095b6070b8bbf587332e26
idrice24 <50774032+idrice24@users.noreply.github.com>
2020-12-04 13:34:05 +0100

    delete app component #359

--- src/app/shared/models/app-comment.ts
+++ src/app/shared/models/app-comment.ts
@@ -1,9 +1,4 @@
 export class Comment {
-// tslint:disable-next-line: max-line-length
-favorite = false;	notablePeople: Person[];
-// tslint:disable-next-line: max-line-length
-constructor(	public name: string,	public code: number, public like: number, public unlike: number, public email: string,	public comment: string,	public previousComment: string	){	this.notablePeople = []; }
+constructor( public comment: string ){ }
 }
 
-export class Person{	name: string;	title: string;	email: string;
-}
