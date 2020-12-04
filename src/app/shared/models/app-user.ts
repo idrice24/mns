0ab5a4d9a1dc30bbe73c46f86e827eef194d6a67
Ghislain <Gzeleu@gmx.de>
2020-12-04 17:59:55 +0100

    Update Api Model for Appuser

--- src/app/shared/models/app-user.ts
+++ src/app/shared/models/app-user.ts
@@ -3,12 +3,15 @@
 export interface AppUser {
   id: number;
   username?: string;
-  fName?: string;
-  lName?: string;
-  email?: string;
-  password?: string;
-  isAdmin?: boolean;
-  avatar?: string;
-  description?: string;
+  fName?: string; // First name of user
+  lName?: string; // Last name of user
+  email?: string; // User email
+  password?: string; // User Password
+  passwordConfirmation?: string;  // User Password Config
+  isAdmin?: boolean; // Indicate Admin role
+  avatar?: string; // Indicate Admin role if Photo missing
   token?: string; // For Token Based Authentication
 }
+
+// API Reference
+// https://www.aviacommerce.org/docs/auth-api.html
