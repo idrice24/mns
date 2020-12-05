18e03cfa50136a065d606c47b2597541dfc7f939
Ghislain <Gzeleu@gmx.de>
2020-12-05 14:08:15 +0100

    #355 Add Overlay to contact

--- src/app/contact/components/contact-map/contact-map.component.ts
+++ src/app/contact/components/contact-map/contact-map.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var google: any;
 @Component({
   selector: 'app-contact-map',
   templateUrl: './contact-map.component.html',
@@ -7,16 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactMapComponent implements OnInit {
   options: any;
+  overlays: any[]; // To display a marker such as circle
 
-  overlays: any[];
+  // To Get longitude and latitude google maps of Makepe city
+  private latitudeMakepe = 4.079478669976131;
+  private logitudeMakepe = 9.721055535221957;
+
+
   constructor() { }
 
 
 
   ngOnInit() {
     this.options = {
-      center: { lat: 36.890257, lng: 30.707417 },
-      zoom: 12
+      center: { lat: this.latitudeMakepe, lng: this.logitudeMakepe },
+      zoom: 17
     };
+
+    this.overlays = [
+      new google.maps.Marker({ position: { lat: this.latitudeMakepe + 1, lng: this.logitudeMakepe + 1 }, title: 'Konyaalti' }),
+      new google.maps.Circle({
+        center: { lat: this.latitudeMakepe + 0.0002, lng: this.logitudeMakepe },
+        fillColor: '#1976D2', fillOpacity: 0.35, strokeWeight: 1, radius: 1500
+      }),
+
+    ];
+
   }
 }
