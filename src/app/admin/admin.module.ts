8d9904011bffb8d0321f588a2641655590e51f40
Ghislain <Gzeleu@gmx.de>
2020-12-17 17:52:38 +0100

    IMPROVE by USE NEW STRATEGY TO IMPORT AD EXPORT EXTERN MODULE  #365

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,40 +7,14 @@ import { ManageGalleryComponent } from './components/manage-gallery/manage-galle
 import { ManageBlogComponent } from './components/manage-blog/manage-blog.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { ManageUserComponent } from './components/manage-user/manage-user.component';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { ManageVideoComponent } from './components/manage-video/manage-video.component';
 import { SharedModule } from '../shared/shared.module';
-import { ProgressSpinnerModule } from 'primeng/progressspinner';
-
-
-import { MenuModule } from 'primeng/menu';
-import { CalendarModule } from 'primeng/calendar';
-import { SliderModule } from 'primeng/slider';
-import { MultiSelectModule } from 'primeng/multiselect';
-import { ContextMenuModule } from 'primeng/contextmenu';
-import { DropdownModule } from 'primeng/dropdown';
-import { ProgressBarModule } from 'primeng/progressbar';
 import { ConfirmationService } from 'primeng/api';
 import { MessageService } from 'primeng/api';
-import { TableModule } from 'primeng/table';
-
-import { RippleModule } from 'primeng/ripple';
-import { ToolbarModule } from 'primeng/toolbar';
-import { DialogModule } from 'primeng/dialog';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { InputNumberModule } from 'primeng/inputnumber';
-
-import { RatingModule } from 'primeng/rating';
-import { ToastModule } from 'primeng/toast';
-import { SidebarModule } from 'primeng/sidebar';
-import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { FileUploadModule } from 'primeng/fileupload';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ManageProductComponent } from './components/manage-product/manage-product.component';
 import { ManageAbonnementComponent } from './components/manage-abonnement/manage-abonnement.component';
+
+
 // REF: https://angular.io/guide/router  live example
 const adminRoutes: Routes = [
   {
@@ -78,35 +52,7 @@ const adminRoutes: Routes = [
   imports: [
     CommonModule,
     SharedModule,
-    ProgressSpinnerModule,
-    TableModule,
-    RippleModule,
-    DialogModule,
-    RatingModule,
-    ToastModule,
-    ButtonModule,
-    InputTextModule,
-    FileUploadModule,
-    TableModule,
-    CalendarModule,
-    SliderModule,
-    DialogModule,
-    MultiSelectModule,
-    ContextMenuModule,
-    DropdownModule,
-    ToastModule,
-    InputTextModule,
-    ProgressBarModule,
-    FileUploadModule,
-    ToolbarModule,
-    RatingModule,
-    InputNumberModule,
-    ConfirmDialogModule,
-    InputTextareaModule,
-    SidebarModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MenuModule,
+
     RouterModule.forChild(adminRoutes)
   ],
   providers: [MessageService, ConfirmationService]
