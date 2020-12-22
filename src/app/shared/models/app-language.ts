88cd79275537ac3d284ec729e27a6847b94e4278
Ghislain <Gzeleu@gmx.de>
2020-12-22 18:45:31 +0100

    #417  Create model to represents country and local id accordig angular theory

---
+++ src/app/shared/models/app-language.ts
@@ -0,0 +1,9 @@
+// Ref
+// https://www.aviacommerce.org/docs/addresses-api.html
+// See Parameters section
+
+export interface AppLanguage {
+  localeIdentifier?: string;
+  countryName?: string;
+
+}
