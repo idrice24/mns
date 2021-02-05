c145469b4ce3c2f74ed017a81a89a4b9bf8925da
idrice24 <50774032+idrice24@users.noreply.github.com>
2021-02-05 12:17:16 +0100

    update files from conflict

--- src/app/media/components/video-list/video-list.component.ts
+++ src/app/media/components/video-list/video-list.component.ts
@@ -7,10 +7,6 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { AppComment } from 'src/app/shared/models/app-comment';
 
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-
-import { AppComment } from 'src/app/shared/models/app-comment';
-
 @Component({
   selector: 'app-video-list',
   templateUrl: './video-list.component.html',
@@ -24,43 +20,25 @@ export class VideoListComponent implements OnInit {
   cols: any[];
   selectedYear;
   public commentForm: FormGroup;
-  public now: Date = new Date();
-
-
-<<<<<<< HEAD
-  public commentForm: FormGroup;
-  public now: Date = new Date(); // declearing the constant now to set the time
+  public now: Date = new Date();// declearing the constant now to set the time
 
   constructor(
         private videoService: VideoService,
     //  private messageService: MessageService, // Fun to use TOAST for  i.e. Comment
         private titleService: Title // Inject to set document title on  browser
-=======
-  constructor(private videoService: VideoService,
-              private titleService: Title // Inject to set document title on  browser
->>>>>>> master
     ) {
     setInterval(() => {
       this.now = new Date();
     }, 1);
      }
-<<<<<<< HEAD
 
      // convenience getter for easy access to form fields
   get author() { return this.commentForm.get('author'); }
   get msg() { return this.commentForm.get('msg'); }
-=======
->>>>>>> master
-
-get msg() { return this.commentForm.get('msg'); }
 
   ngOnInit(): void {
     this.getVideos();
 
-<<<<<<< HEAD
-=======
-
->>>>>>> master
     this.cols = [
       { field: 'top', header: 'Top' },
       { field: 'name', header: 'Nom' },
@@ -79,13 +57,10 @@ get msg() { return this.commentForm.get('msg'); }
     this.titleService.setTitle('MNS237 - Videos'); // Adding the title Home to the MNS237  main Title
 
     this.commentForm = new FormGroup({
-<<<<<<< HEAD
       author: new FormControl('', [
         Validators.required,
         Validators.minLength(4),
       ]),
-=======
->>>>>>> master
       msg: new FormControl('', [
         Validators.required,
         Validators.minLength(5)])
@@ -115,7 +90,6 @@ get msg() { return this.commentForm.get('msg'); }
 
   postYourComment(theData) {
     const message = theData.msg;
-<<<<<<< HEAD
     const author = theData.author;
 
     // @Idrice: this is how to create a object of type Comment
@@ -123,11 +97,6 @@ get msg() { return this.commentForm.get('msg'); }
       author: theData.author,
       createdDate: this.now,
       id: this.selectedVideo.comments.length,
-=======
-
-    const commentObject: AppComment = {
-      createdDate: this.now,
->>>>>>> master
       msg: message
     };
 
@@ -137,16 +106,13 @@ get msg() { return this.commentForm.get('msg'); }
 
     // Tell the serice to update the topic in Server due to added Comments
     // @Idrice: we have to call subscribe() here to fire the method
-<<<<<<< HEAD
     // this.videoService.updateVideo(this.selectedVideo).subscribe();
 /*
     this.messageService.add({
       severity: 'success', summary: 'Commentaire ajoute',
       detail: author + ' Says ' + message
     }); // this is the pop message to confirm the comment send*/
-=======
-    this.videoService.updateTopic(this.selectedVideo).subscribe();
->>>>>>> master
+
 
     this.commentForm.reset(); // Clean the Form
   }
