2a2fc4e17daafba400e968c7049d12e17a888b97
idrice24 <50774032+idrice24@users.noreply.github.com>
2021-03-20 08:17:01 +0100

    update photo service to get all photos #436

--- src/app/shared/services/photo.service.ts
+++ src/app/shared/services/photo.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Image } from '../models/image';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,8 @@ export class PhotoService {
 
   getImages() {
     return this.httpClient.get<Image[]>(this.imageUrl);
+  }
+  getPhotos() {
+    return this.httpClient.get<Image[]>(this.imageUrl).pipe(map(res => res));
   }
 }
