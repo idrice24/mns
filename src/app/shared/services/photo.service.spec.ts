bf5aaa89695e2763d1a75de09e63e45d48d98ebd
idrice24 <50774032+idrice24@users.noreply.github.com>
2021-03-26 18:58:17 +0100

    update photo service for gallery management #436

--- src/app/shared/services/photo.service.spec.ts
+++ src/app/shared/services/photo.service.spec.ts
@@ -1,13 +1,14 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { PhotoService } from './photo.service';
 
 describe('PhotoService', () => {
   let service: PhotoService;
-
+  let httpClient: HttpClient;
 
 
 
@@ -19,6 +20,7 @@ describe('PhotoService', () => {
     });
 
     service = TestBed.inject(PhotoService);
+    httpClient = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
