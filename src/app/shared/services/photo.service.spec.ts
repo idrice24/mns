979825027db8a65f5e0b9c634bad527bb16cd918
idrice24 <50774032+idrice24@users.noreply.github.com>
2021-04-01 07:25:13 +0100

    update time #439

--- src/app/shared/services/photo.service.spec.ts
+++ src/app/shared/services/photo.service.spec.ts
@@ -1,14 +1,13 @@
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { PhotoService } from './photo.service';
 
 describe('PhotoService', () => {
   let service: PhotoService;
-  let httpClient: HttpClient;
 
 
 
@@ -20,7 +19,6 @@ describe('PhotoService', () => {
     });
 
     service = TestBed.inject(PhotoService);
-    httpClient = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
