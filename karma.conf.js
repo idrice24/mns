565a9d4835dd48989d74f48836b9dfec0e640822
Ghislain <gzeleu@outlook.de>
2021-05-01 00:04:33 +0200

    Circleci (#465)
    * add yml and removed circle ci #461
    
    * !dd lin uin read me
    
    * Karma confi
    
    Co-authored-by: Ghislain <Gzeleu@gmx.de>

--- karma.conf.js
+++ karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS','Chrome', 'ChromeHeadless'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
