770f1bb55a1384fa939f772e7c48e3df288a305b
idrice24 <50774032+idrice24@users.noreply.github.com>
2021-11-12 20:12:05 +0100

    update admin dashboard #394
    this is redesign the board to show sweet UI

--- src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -49,6 +49,10 @@ export class AdminDashboardComponent implements OnInit {
   message: string;
   public now: Date = new Date();
 
+  basicData: any;
+  basicOptions: any;
+  chartOptions: any;
+
 
   constructor(
     private logService: LogService,
@@ -72,6 +76,23 @@ export class AdminDashboardComponent implements OnInit {
 
       this.loggingAvatar = this.avatarService.generateAvatar(el, 'zaba');
       this.logginUserId = this.userService.getUsers();
+
+
+      this.basicData = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'My First dataset',
+                    backgroundColor: '#42A5F5',
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: 'My Second dataset',
+                    backgroundColor: '#FFA726',
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        };
 
     });
 
