6490a95cec8ba67c6614cd73e3149e8329e2f9b8
Ghislain <Gzeleu@gmx.de>
2022-01-16 22:58:23 +0100

    Node

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -29,8 +25,8 @@ import { MessageService } from 'primeng/api';
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
-  //  HttpClientInMemoryWebApiModule.forRoot(
-   //   InMemoryDataService, { dataEncapsulation: false }),
+    //  HttpClientInMemoryWebApiModule.forRoot(
+    //   InMemoryDataService, { dataEncapsulation: false }),
     SharedModule,
     AppRoutingModule, // CLI adds AppRoutingModule to the AppModule's imports array
 
