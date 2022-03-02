c097d1dae68f0e0d15748a9dd07066ec657e2bea
Ghislain <gzeleu@outlook.de>
2022-03-02 19:01:15 +0100

    [PR} Fixed Bug 497 (#500)
    * Update bulde auto #497
    
    * Move cms to hisown moduel #497
    
    * Try to cerate bulndel indo doc fol #497
    
    Co-authored-by: Ghislain <Gzeleu@gmx.de>

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,48 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistryComponent } from './components/registry/registry.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+import { CoreModule } from '../core/core.module';
+
+import { CardModule } from 'primeng/card';
+import { ContactModule } from '../contact/contact.module';
+
+const authRoutes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'registry',
+    component: RegistryComponent
+  },
+]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  declarations: [
+    LoginComponent,
+    RegistryComponent
   ],
-  exports: []
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SharedModule,
+    //CoreModule,
+    // CardModule,
+    // ContactModule, // Due to app-contact-form component in home
+    RouterModule.forChild(authRoutes) // imports router def for this module
+  ],
+  exports: [
+    RouterModule // to make auth routes externally
+  ]
 })
 export class AuthModule { }
