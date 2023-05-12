2cade87055a59c74bc2da0e9200adf87f1e25854
idrice24 <idrice.tsafouet@yahoo.com>
2023-05-12 21:24:54 +0100

    update product #501

--- src/app/product/components/product-category/product-category.component.ts
+++ src/app/product/components/product-category/product-category.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { ProductService } from 'src/app/shared/services/product.service';
 import { Title } from '@angular/platform-browser';
+import { Product } from 'src/app/shared/models/product';
+import { Observable } from 'rxjs';
 
 @Component({
 	selector: 'app-product-category-view',
@@ -10,6 +12,39 @@ import { Title } from '@angular/platform-browser';
 })
 
 export class ProductCategoryComponent implements OnInit {
-	products: any;
-	ngOnInit(): void {}
+	
+	products$: Observable<Product[]>; // @Idirce see stocts$ in p.
+  	products: Product[];
+  	responsiveOptions;
+
+  	constructor(
+  		private productService: ProductService,
+  		private messageService: MessageService // To invoke toast message
+  		){
+  		this.responsiveOptions = [
+			{
+			breakpoint: '1024px',
+			numVisible: 3,
+			numScroll: 3
+			},
+			{
+			breakpoint: '768px',
+			numVisible: 2,
+			numScroll: 2
+			},
+			{
+			breakpoint: '560px',
+			numVisible: 1,
+			numScroll: 1
+			}
+    ];
+  	}
+	ngOnInit(): void {
+		this.products$ = this.productService.getProducts();
+	}
+	addToChart(product) {
+    console.log(product.id);
+    this.messageService.addAll([{ severity: 'success', summary: 'Service Message', detail: 'Via MessageService' },
+    { severity: 'info', summary: '@idrice Todo', detail: 'TODO' }]);
+  }
 }
